2e9f73d434634a8ce53e51a484bd0dcab68f2e61
Vladyslav Tkachenko <vlad@fireblink.com>
2019-04-09 12:48:43 +0300

    fix(backup.sh): wrong env var used in debug echo command

--- backup.sh
+++ backup.sh
@@ -9,7 +9,7 @@ BACKUP_NAME="mysqldump_$(date -u +%Y-%m-%d_%H-%M-%S).sql.gz"
 [ -z "${MYSQL_DB_NAME}" ] && { echo "Please specify MYSQL_DB_NAME" > /dev/stderr; exit 1;}
 [ -z "${S3_BUCKET}" ] && { echo "Please specify S3_BUCKET" > /dev/stderr; exit 1;}
 
-echo "Backing up mysql: user=${MYSQL_USER}, host=${MYSQL_HOST}, port=${MYSQL_PORT} , db=${DATABASE_NAME} to s3://${S3_BUCKET}/${BACKUP_NAME}"
+echo "Backing up mysql: user=${MYSQL_USER}, host=${MYSQL_HOST}, port=${MYSQL_PORT}, db=${MYSQL_DB_NAME} to s3://${S3_BUCKET}/${BACKUP_NAME}"
 
 mysqldump --verbose -u "${MYSQL_USER}" -p"${MYSQL_PASS}" -h "${MYSQL_HOST}" --port "${MYSQL_PORT}" --databases ${MYSQL_DB_NAME} | gzip | aws s3 cp - s3://${S3_BUCKET}/${BACKUP_NAME}
 
